b6307dc803b845a7fd484d03f0591fbb6a40917a
Markijani <markijani55@gmail.com>
2023-08-10 15:55:56 -0700

    Initial commit

---
+++ src/main/java/us.kvitko.calcapp/_main/_Main.java
@@ -0,0 +1,23 @@
+package us.kvitko.calcapp._main;
+
+import us.kvitko.calcapp.service.ResultWriteService;
+
+import us.kvitko.calcapp.util.Calculator;
+
+public class _Main {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int result = Calculator.add(num1, num2);
+        int result1 = Calculator.subtract(num1, num2);
+        int result2 = Calculator.multiply(num1, num2);
+
+        ResultWriteService.printResult(num1, num2, "addition", result);
+        ResultWriteService.printResult1(num1, num2, "subtraction", result1);
+        ResultWriteService.printResult2(num1, num2, "multiplication", result2);
+}
+
+}
+
