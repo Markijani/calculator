d2b71811c980014e1e2cd9142784a6673a6d48b2
Markijani <markijani55@gmail.com>
2023-08-12 23:05:26 -0700

    Update2

--- src/main/java/us.kvitko.calcapp/_main/_Main.java
+++ src/main/java/us.kvitko.calcapp/_main/_Main.java
@@ -11,9 +11,10 @@ public class _Main {
         int result = Calculator.add(num1, num2);
         int result1 = Calculator.subtract(num1, num2);
         int result2 = Calculator.multiply(num1, num2);
-        ResultWriteService.printResult(num1, num2, "addition", result);
-        ResultWriteService.printResult1(num1, num2, "subtraction", result1);
-        ResultWriteService.printResult2(num1, num2, "multiplication", result2);
+        ResultWriteService.printResult(num1, num2, "addition",result);
+        ResultWriteService.printResult(num1, num2, "subtraction",result1);
+        ResultWriteService.printResult(num1, num2, "multiplication",result2);
+
     }
 
 }
