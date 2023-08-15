427218ccba49e83bbf8c7db5d182c4f427c4c05e
Markijani <markijani55@gmail.com>
2023-08-14 19:56:05 -0700

    fix _Main.java

--- src/main/java/us.kvitko.calcapp/_main/_Main.java
+++ src/main/java/us.kvitko.calcapp/_main/_Main.java
@@ -1,18 +1,18 @@
 package us.kvitko.calcapp._main;
 
 import us.kvitko.calcapp.service.ResultWriteService;
-import us.kvitko.calcapp.util.Calculator;
+import static us.kvitko.calcapp.util.Calculator.*;
 
 public class _Main {
 
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 5;
-        int result = Calculator.add(num1, num2);
-        result = Calculator.subtract(num1, num2);
-        result = Calculator.multiply(num1, num2);
+        int result = add(num1, num2);
         ResultWriteService.printResult(num1, num2, "addition", result);
+        result = subtract(num1, num2);
         ResultWriteService.printResult(num1, num2, "subtraction", result);
+        result = multiply(num1, num2);
         ResultWriteService.printResult(num1, num2, "multiplication", result);
     }
 
--- src/main/java/us.kvitko.calcapp/service/ResultWriteService.java
+++ src/main/java/us.kvitko.calcapp/service/ResultWriteService.java
@@ -2,8 +2,8 @@ package us.kvitko.calcapp.service;
 
 public class ResultWriteService {
 
-    public static void printResult(int num1, int num2, String operationName, int result) {
-        System.out.println(num1 + " " + num2 + " " + operationName + " " + result);
+    public static void printResult(int num1, int num2, String operationName, int customResult) {
+        System.out.println(num1 + " " + num2 + " " + operationName + " " + customResult);
     }
 
 }
